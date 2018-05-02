2969714800acdc9ac57e523b065ba700f2da2685
Gabriel Nourrit <nourritg@F202-13.e-im2ag.ujf-grenoble.fr>
2018-05-02 17:02:42 +0200

    back to the future : debut de la boite mail sisipizilemonskizi

---
+++ formulaire/src/application/Sondage/BoutonChoix.java
@@ -0,0 +1,44 @@
+package application.Sondage;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.HBox;
+
+public class BoutonChoix extends HBox{
+	private Label nom;
+	private RadioButton bool;
+	private Boolean isTrue;
+	protected HBox compoBC;
+	
+	BoutonChoix(String nom){
+		this.nom = new Label(nom);
+		genererSousComposant();
+		ecouteurDefaultAction();
+		layoutDefaultParametre();
+	}
+	
+	protected void genererSousComposant(){
+		this.bool = new RadioButton();
+		this.compoBC = new HBox();
+		this.isTrue=false;
+	}
+	
+	protected void ecouteurDefaultAction() {
+		this.bool.setOnAction(event ->{
+			this.isTrue = !(this.isTrue);
+		});
+	}
+	
+	protected void layoutDefaultParametre() {
+		compoBC.getChildren().addAll(nom,bool);
+		compoBC.setMaxSize(120, 100);
+		compoBC.setSpacing(3);
+		compoBC.setAlignment(Pos.CENTER);
+		this.getChildren().add(compoBC);
+	}
+	
+	public Boolean checked() {
+		return new Boolean(isTrue);
+	}
+}
